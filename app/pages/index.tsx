3cac2ae5a074db9526ca0ac0b6ee79cacb2a7826
Cristian Deleon <libaration@gmail.com>
2021-12-08 03:16:34 -0500

    mutations add to sqlite database in relation to rooms

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react"
-import { Link, Routes } from "blitz"
+import { Link, useMutation } from "blitz"
+import createUser from "app/users/mutations/createUser"
 const Home: BlitzPage = () => {
   const [name, setName] = useState("")
+  const [createUserMutation] = useMutation(createUser)
   return (
     <div className="flex flex-col items-center text-center pt-5">
-      Enter your name to create a room
+      Enter your name {name} to create a room
       <br />
-      <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+      <form
+        className="text-black"
+        onSubmit={async (e) => {
+          e.preventDefault()
+          if (window.sessionStorage.getItem("id")) return
+          const user = await createUserMutation({ name: `${name}`, roomNumber: 1 })
+          window.sessionStorage.setItem("id", user!.id.toString())
+        }}
+      >
         <input
           type="text"
           value={name}
@@ -14,8 +24,10 @@ const Home: BlitzPage = () => {
           className="pl-5 mr-5 pr-5 rounded-md bg-gray-200"
           maxLength={16}
         />
-        Create Raid Room
-      </button>
+        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+          Create Raid Room
+        </button>
+      </form>
       <br />
       <Link href="#">.... already know your raids room number?</Link>
     </div>
--- app/users/mutations/createUser.ts
+++ app/users/mutations/createUser.ts
@@ -1,14 +1,17 @@
 import db from "db"
-export default async function createUser(name, roomNumber) {
-  const existing = await db.room.findMany({ where: { id: roomNumber } })
+export default async function createUser(values) {
+  let user
+  const existing = await db.room.findMany({ where: { id: values.roomNumber } })
   if (!existing.length) {
-    await db.room.create({ data: { id: roomNumber } })
+    await db.room.create({ data: { id: values.roomNumber } })
+    user = await db.user.create({
+      data: {
+        roomId: values.roomNumber,
+        name: values.name,
+      },
+    })
+  } else {
+    user = await db.user.create({ data: { name: values.name, roomId: values.roomNumber } })
   }
-  const user = await db.user.create({
-    data: {
-      room: roomNumber,
-      name: name,
-    },
-  })
   return user
 }
